5d3b06ce0530a1958326bab808e8c7b03280be03
truco23 <trucopaulista23@gmail.com>
2020-04-27 14:46:34 -0300

    feat(commitzen): commitzen

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Teste1 />
-      <Teste2 />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
